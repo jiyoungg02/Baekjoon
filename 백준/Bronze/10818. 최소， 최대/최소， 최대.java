cac8554eb2292f3556da6cb5b1ed96e6d873658f
jiyoungg02 <jiyoung_g02@naver.com>
2025-05-13 13:42:36 +0900

    [Bronze III] Title: 최소, 최대, Time: 396 ms, Memory: 120312 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,16 +8,23 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 		
 		int[] arr = new int[N];
 		
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
+			
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+			
+			if(min > arr[i]) {
+				min = arr[i];
+			}
 		}
 		
-		Arrays.sort(arr);
-
-		System.out.println(arr[0] + " " + arr[arr.length-1]);
-		
+		System.out.println(min + " " + max);
 	}
 }
