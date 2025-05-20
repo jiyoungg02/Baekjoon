569a41e5ebaf8088bb587034f219f85bfc1da009
jiyoungg02 <jiyoung_g02@naver.com>
2025-05-20 11:31:44 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 68 ms, Memory: 11736 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i; j++) {
+				sb.append(" ");
+			}
+			for(int j=1; j<=i*2-1; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		for(int i=n-1; i>=0; i--) {
+			for(int j=n-i; j>0; j--) {
+				sb.append(" ");
+			}
+			for(int j=i*2-1; j>=1; j--) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+}
