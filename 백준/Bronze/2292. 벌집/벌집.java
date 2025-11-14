f24c45ca12c00841b6de34516fc95fcdc404261e
jiyoungg02 <jiyoung_g02@naver.com>
2025-11-14 15:31:04 +0900

    [Bronze II] Title: 벌집, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -7,21 +7,17 @@ public class Main {
         
         int N = Integer.parseInt(br.readLine());
         int count = 1;
-        int range = 1;
+        int range = 2;
         
         if(N == 1){
             System.out.println(1);
         }
         else {
-            while(true) {
-                range = range + 6 * count;
-                if(N <= range){
-                    System.out.println(count + 1);
-                    break;
-                } else {
-                    count++;
-                }
+            while(range <= N) {
+                range = range + (6 * count);
+                count++;
             }
+            System.out.println(count);
         } 
         
     }
